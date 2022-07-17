03fe5e74fee57711f678f2329203a4d9dc9cefd5
Forhad-Khan-Alif <81805841+Forhad-Khan-Alif@users.noreply.github.com>
2022-07-17 16:55:45 +0600

    add operators

--- js/script.js
+++ js/script.js
@@ -1,8 +1,58 @@
+// Js Syntax ...........................................................
 /*var x = 10;
 var y = 5;
 z = x+y;*/
 
 // console.log(z); 
 // document.getElementById('js_syntax').innerHTML= z;
+
 // Single Line Comment
-/* Multiple Line Comment*/
+/* Multiple Line Comment....*/
+
+
+// Variable_VAR_LET_CONST................................................
+/*let x = 90;
+let y = 10;
+let z = x+y;
+document.getElementById('variable_VAR_LET_CONST_ES6').innerHTML= z ; ....*/
+
+// Js Operators ..........................................................
+// let messiAge = 22;
+// let otamendiAge = 28;
+// let totalAge = messiAge+otamendiAge;
+
+// document.getElementById('Js_operators').innerHTML= totalAge;
+// console.log(messiAge);
+
+// let messiAge = 22;
+// messiAge +=4;
+// document.getElementById('Js_operators').innerHTML= messiAge;
+
+// let nameString = 'Forhad Khan';
+// document.getElementById('Js_operators').innerHTML= nameString.length;
+
+// let nameString = 'Forhad Khan';
+// document.getElementById('Js_operators').innerHTML= nameString.toUpperCase();
+
+// let nameString = 'Forhad Khan';
+// document.getElementById('Js_operators').innerHTML= nameString.substring(0, 6);
+
+// let nameString = 'Forhad Khan';
+// document.getElementById('Js_operators').innerHTML= nameString.substring(4);
+
+// let nameString = 'Forhad Khan';
+// document.getElementById('Js_operators').innerHTML= nameString.split('');
+
+// let firstName = 'Forhad';
+// let lastName = prompt('What is your last name?');
+// let age = prompt('What is your age?');
+// document.getElementById('Js_operators').innerHTML= firstName + ' ' + lastName + ' ' + 'I am' +  ' ' + age + ' ' + 'Years Old.';
+// console.log(firstName + ' ' + lastName + ' ' + age);
+
+let firstName = prompt('What is your first name?');
+let lastName = prompt('What is your last name?');
+let age = prompt('What is your age?');
+document.getElementById('Js_operators').innerHTML='I am' + ' ' + firstName + ' ' + lastName + '. ' + 'I am' +  ' ' + age + ' ' + 'years old.';
+console.log(firstName + ' ' + lastName + ' ' + age);
+
+
