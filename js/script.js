af51a55fc5b658b0c6ce0fbab9415822e4649e76
Forhad-Khan-Alif <81805841+Forhad-Khan-Alif@users.noreply.github.com>
2022-07-18 12:04:19 +0600

    Js Operators, Data types add

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ z = x+y;*/
 /*let x = 90;
 let y = 10;
 let z = x+y;
-document.getElementById('variable_VAR_LET_CONST_ES6').innerHTML= z ; ....*/
+document.getElementById('variable_VAR_LET_CONST_ES6').innerHTML= z ;*/
 
 // Js Operators ..........................................................
 // let messiAge = 22;
@@ -49,10 +49,80 @@ document.getElementById('variable_VAR_LET_CONST_ES6').innerHTML= z ; ....*/
 // document.getElementById('Js_operators').innerHTML= firstName + ' ' + lastName + ' ' + 'I am' +  ' ' + age + ' ' + 'Years Old.';
 // console.log(firstName + ' ' + lastName + ' ' + age);
 
-let firstName = prompt('What is your first name?');
-let lastName = prompt('What is your last name?');
-let age = prompt('What is your age?');
-document.getElementById('Js_operators').innerHTML='I am' + ' ' + firstName + ' ' + lastName + '. ' + 'I am' +  ' ' + age + ' ' + 'years old.';
-console.log(firstName + ' ' + lastName + ' ' + age);
+// let firstName = prompt('What is your first name?');
+// let lastName = prompt('What is your last name?');
+// let age = prompt('What is your age?');
+// document.getElementById('Js_operators').innerHTML='I am' + ' ' + firstName + ' ' + lastName + '. ' + 'I am' +  ' ' + age + ' ' + 'years old.';
+// console.log(firstName + ' ' + lastName + ' ' + age);
+
+// Js Data types ..........................................................
+
+// let phone = 'OnePlus';
+// let model = 7;
+// let model = 7.7;
+// let phone = 'OnePlus' + 7;
+// let phone = 7 + 8 + 'OnePlus';
+// let phone = 'OnePlus' + 7 + 8;
+// let phone = "Hello World. 'My name is Forhad Khan'";
+// let phone = 'Hello World. "My name is Forhad Khan"';
+// let phone = 7;
+// let mobile = 8;
+
+// Js Array...............................................................
+// let phone = ['Apple', 'Xiaomi', 'Oneplus'];
+// document.getElementById('Js_Data_types').innerHTML= phone;
+// console.log(phone);
+
+// let phone = ['Apple', 'Xiaomi', 'Oneplus'];
+// document.getElementById('Js_Data_types').innerHTML= phone[1];
+// console.log(phone);
+
+// let phone = ['Apple', 'Xiaomi', 'Oneplus'];
+// phone[3] = 'Nokia';
+// document.getElementById('Js_Data_types').innerHTML= phone[1];
+// console.log(phone);
+
+// Js Object..............................................................
+// let phone = {
+//     model: 'iPhone 13',
+//     storage: '164GB',
+//     color: 'Black',
+// };
+// document.getElementById('Js_Data_types').innerHTML= phone;
+// console.log(phone);
+
+// let phone = {
+//     model: 'iPhone 18',
+//     storage: '164GB',
+//     color: 'Black',
+// };
+// document.getElementById('Js_Data_types').innerHTML= phone.model;
+// console.log(phone);
+
+// let phone = {
+//     model: 'iPhone 18',
+//     storage: '164 GB',
+//     color: 'Black',
+// };
+// phone.storage = '256 GB';
+// document.getElementById('Js_Data_types').innerHTML= phone.storage;
+// console.log(phone);
+
+// Js null, undefined...........................................................
+// let phone;
+// document.getElementById('Js_Data_types').innerHTML= phone;
+// console.log(phone);
+
+// let phone = null;
+// document.getElementById('Js_Data_types').innerHTML= phone;
+// console.log(phone);
+
+// let phone = null;
+// document.getElementById('Js_Data_types').innerHTML= null == undefined;
+// console.log(phone);
+
+let phone = null;
+document.getElementById('Js_Data_types').innerHTML= null === undefined;
+console.log(phone);
 
 
